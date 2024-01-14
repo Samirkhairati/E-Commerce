cf4f7ee2fe9b51aba978857d9f8f77d730891f49
Samirkhairati <samirk10101@gmail.com>
2024-01-15 00:50:46 +0530

    chore: install Flowbite for styling and UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import Navigation from './pages/auth/Navigation'
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './app.css'
+
+import { Button } from 'flowbite-react';
 
 function App() {
 
@@ -13,11 +15,22 @@ function App() {
       <main className='py-33'>
         <Outlet />
       </main>
+      <div className="flex flex-wrap gap-2">
+        <Button>Default</Button>
+        <Button color="red">Blue</Button>
+        <Button color="gray">Gray</Button>
+        <Button color="dark">Dark</Button>
+        <Button color="light">Light</Button>
+        <Button color="success">Success</Button>
+        <Button color="failure">Failure</Button>
+        <Button color="warning">Warning</Button>
+        <Button color="purple">Purple</Button>
+      </div>
     </>
   )
 }
 
-export default App 
+export default App
 
 
 
