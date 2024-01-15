722297d534b4bf0b198b8400a432549de34aed2a
Samirkhairati <samirk10101@gmail.com>
2024-01-15 10:52:17 +0530

    build: install Flowbite for styling and UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import './app.css'
 
 import { Button } from 'flowbite-react';
+import { Carousel } from 'flowbite-react';
 
 function App() {
 
@@ -25,6 +26,15 @@ function App() {
         <Button color="failure">Failure</Button>
         <Button color="warning">Warning</Button>
         <Button color="purple">Purple</Button>
+      </div>
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel>
+          <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        </Carousel>
       </div>
     </>
   )
