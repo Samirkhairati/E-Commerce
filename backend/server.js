d5682e679f42f72ada588164d5fd471e5e0e167b
Samirkhairati <samirk10101@gmail.com>
2024-01-24 03:54:01 +0530

    build: vite build

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: 'http://localhost:3000', //localhost:5173 for development
+        origin: 'https://e-commerce-production-ecfb.up.railway.app', //localhost:5173 for development || localhost:3000 for build
         credentials: true,
     }
 ));
