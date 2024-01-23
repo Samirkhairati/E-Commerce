603a66e6dfae676e8e56984cc2d7379f885151ef
Samirkhairati <samirk10101@gmail.com>
2024-01-24 04:23:58 +0530

    style: favicon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api/": 'https://e-commerce-production-8ee5.up.railway.app',
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api/": 'https://e-commerce-production-8ee5.up.railway.app',
+    },
+  },
 });
