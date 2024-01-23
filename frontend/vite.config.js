9b6eae84518fac3134996c840239d832fb3b4d9d
Samirkhairati <samirk10101@gmail.com>
2024-01-24 03:09:16 +0530

    build: frontend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": process.env.BACKEND_URL || "http://localhost:6969",
+      "/api/": 'https://e-commerce-production-8ee5.up.railway.app',
     },
   },
 });
