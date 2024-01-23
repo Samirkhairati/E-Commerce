d72935d99809f778dfe5df35e49b87ba4f5ee11e
Samirkhairati <samirk10101@gmail.com>
2024-01-24 04:51:44 +0530

    fix: dotenv frontend

--- frontend/src/actions/constants.js
+++ frontend/src/actions/constants.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const BASE_URL = process.env.BACKEND_URL || 'http://localhost:6969'
+export const BASE_URL = proccess.env.BACKEND_URL || ''
 export const USERS_URL = `${BASE_URL}/api/users`
