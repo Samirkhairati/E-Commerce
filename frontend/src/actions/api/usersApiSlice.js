dd6db4a70932decbdb60cab306776ae08790e592
Samirkhairati <samirk10101@gmail.com>
2024-01-25 04:30:04 +0530

    feat: product, order, category models

--- frontend/src/actions/api/usersApiSlice.js
+++ frontend/src/actions/api/usersApiSlice.js
@@ -64,7 +64,7 @@ export const {
   useLoginMutation,
   useRegisterMutation,
   useLogoutMutation,
-//   useProfileMutation,
+  useProfileMutation,
 //   useGetUsersQuery,
 //   useDeleteUserMutation,
 //   useUpdateUserMutation,
