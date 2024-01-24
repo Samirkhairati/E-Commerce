25b5b07a1c23b5820831386838eab89044979830
Samirkhairati <samirk10101@gmail.com>
2024-01-25 04:46:43 +0530

    feat: backend category controller

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,218 +1,81 @@
-// goes over createUser, loginUser, logoutUser;
+import Category from "../models/categoryModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
 
-import User from '../models/userModel.js';
-import asyncHandler from '../middleware/asyncHandler.js';
-import bcrypt from 'bcryptjs';
-import createToken from '../utils/createToken.js';
+const createCategory = asyncHandler(async (req, res) => {
+    const { name } = req.body;
 
-const createUser = asyncHandler(async (req, res, next) => {
-
-    // fields check
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
+    if (!name) {
         res.status(400);
         throw new Error('@createUser ERORR: Please provide all fields');
     }
 
-    // check if registered
-    const userExists = await User.findOne({ email });
-    if (userExists) {
+    const existingCategory = await Category.findOne({ name });
+
+    if (existingCategory) {
+        res.status(400);
+        throw new Error('@createUser ERORR: Category already exists');
+    }
+
+    const category = await new Category({ name }).save();
+    res.json(category);
+}, "@createCategory ERROR: definition: ");
+
+const updateCategory = asyncHandler(async (req, res) => {
+    try {
+        const { name } = req.body;
+        const { categoryId } = req.params;
+
+        const category = await Category.findOne({ _id: categoryId });
+
+        if (!category) {
+            res.status(400);
+            throw new Error('@updateCategory ERROR: Category does not exist');
+        }
+
+        category.name = name;
+
+        const updatedCategory = await category.save();
+        res.json(updatedCategory);
+    } catch (error) {
         res.status(400)
         throw new Error('@createUser ERROR: User already exists');
     }
+}, "@updateCategory ERROR: definition: ");
 
-    //hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // upload to db
-    const user = new User({ username, email, password: hashedPassword });
+const removeCategory = asyncHandler(async (req, res) => {
     try {
-        await user.save();
-        createToken(res, user._id);
-        console.log('lessgo1')
-        res.status(201).json({
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-            password: user.password,
-        })
-        console.log('lessgo2')
+        const removed = await Category.findByIdAndRemove(req.params.categoryId);
+        res.status(200).json(removed);
     } catch (error) {
-        res.status(400);
-        throw new Error('@createUser ERROR: User could not be created');
+        res.status(500)
+        throw new Error('@removeCategory ERROR: cannot find category');
     }
+}, "@removeCategory ERROR: definition: ");
 
-
-    next();
-
-}, '@createUser ERROR: definition: ')
-
-const loginUser = asyncHandler(async (req, res, next) => {
-
-    const { email, password } = req.body;
-
-    // fields check
-    if (!email || !password) {
-        res.status(400);
-        throw new Error('@loginUser ERROR: Please provide all fields');
+const listCategory = asyncHandler(async (req, res) => {
+    try {
+        const all = await Category.find({});
+        res.json(all);
+    } catch (error) {
+        res.status(500)
+        throw new Error('@listCategory ERROR: cannot find category');
     }
+}, "@listCategory ERROR: definition: ");
 
-    if (!email.includes('@') || !email.includes('.')) {
-        res.status(400);
-        throw new Error('@loginUser ERROR: Please provide a valid email');
+const readCategory = asyncHandler(async (req, res) => {
+    try {
+        const category = await Category.findOne({ _id: req.params.id });
+        res.json(category);
+    } catch (error) {
+        res.status(500)
+        throw new Error('@readCategory ERROR: cannot find category');
     }
-
-    // check if registered
-    const userExists = await User.findOne({ email });
-    if (!userExists) {
-        res.status(400);
-        throw new Error('@loginUser ERROR: User does not exist');
-    } else {
-        const passwordMatch = await bcrypt.compare(password, userExists.password);
-        if (!passwordMatch) {
-            res.status(400);
-            throw new Error('@loginUser ERROR: Password is incorrect');
-        } else {
-            createToken(res, userExists._id);
-            res.status(200).json({
-                _id: userExists._id,
-                username: userExists.username,
-                email: userExists.email,
-                password: userExists.password,
-            });
-        }
-    }
-    next();
-
-}, '@loginUser ERROR: definition: ')
-
-const logoutUser = asyncHandler(async (req, res, next) => {
-
-    res.cookie('jwt', '', {
-        httpOnly: true,
-        expires: new Date(0),
-    });
-
-    res.status(200).json({ message: 'User logged out', });
-
-    next();
-
-}, '@logoutUser ERROR: definition: ')
-
-const getAllUsers = asyncHandler(async (req, res, next) => {
-
-    const users = await User.find({});
-    res.status(200).json(users);
-    next();
-
-}, '@getAllUsers ERROR: definition: ')
-
-const getCurrentUserProfile = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user._id);
-
-    if (user) {
-        res.status(200).json({
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-        });
-    } else {
-        res.status(404);
-        throw new Error('@getCurrentUserProfile ERROR: User not found');
-    }
-}, '@getCurrentUserProfile ERROR: definition: ')
-
-const updateCurrentUserProfile = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user._id);
-    if (user) {
-
-        user.username = req.body.username || user.username;
-        user.email = req.body.email || user.email;
-
-        if (req.body.password) {
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(req.body.password, salt);
-        } 
-
-        const updatedUser = await user.save();
-
-        res.status(200).json({
-            _id: updatedUser._id,
-            username: updatedUser.username,
-            email: updatedUser.email,
-            isAdmin: updatedUser.isAdmin,
-        })
-        // next()
-    } else {
-        res.status(404);
-        throw new Error('@updateCurrentUserProfile ERROR: User not found');
-    }
-
-}, '@updateCurrentUserProfile ERROR: definition: ')
-
-const deleteUserById = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.id);
-
-    if (user) {
-        if (!user.isAdmin) {
-            await User.deleteOne({ _id: req.params.id });
-            res.status(200).json({ message: 'User removed' });
-            next();
-        } else {
-            res.status(400);
-            throw new Error('@deleteUserById ERROR: Cannot delete admin user');
-        }
-    } else {
-        res.status(404);
-        throw new Error('@deleteUserById ERROR: User not found');
-    }
-
-}, '@deleteUserById ERROR: definition: ')
-
-const getUserById = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.id).select('-password');
-
-    if (user) {
-        res.status(200).json(user);
-    } else {
-        res.status(404);
-        throw new Error('@getUserById ERROR: User not found');
-    }
-
-}, '@getUserById ERROR: definition: ')
-
-const updateUserById = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-        user.username = req.body.username || user.username;
-        user.email = req.body.email || user.email;
-        user.isAdmin = req.body.isAdmin || user.isAdmin;
-
-        const updatedUser = await user.save();
-
-        res.status(200).json({
-            _id: updatedUser._id,
-            username: updatedUser.username,
-            email: updatedUser.email,
-            isAdmin: updatedUser.isAdmin,
-        });
-        next()
-    } else {
-        res.status(404);
-        throw new Error('@updateUserById ERROR: User not found');
-    }
-
-}, '@updateUserById ERROR: definition: ')
+}, "@readCategory ERROR: definition: ");
 
 export {
-    createUser,
-    loginUser,
-    logoutUser,
-    getAllUsers,
-    getCurrentUserProfile,
-    updateCurrentUserProfile,
-    deleteUserById,
-    getUserById,
-    updateUserById,
+    createCategory,
+    updateCategory,
+    removeCategory,
+    listCategory,
+    readCategory,
 };
