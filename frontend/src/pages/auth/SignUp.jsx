9c3f3bf459f11734965872f43893d23786fa3251
Samirkhairati <samirk10101@gmail.com>
2024-01-24 06:21:08 +0530

    style: register loader

--- frontend/src/pages/auth/SignUp.jsx
+++ frontend/src/pages/auth/SignUp.jsx
@@ -113,7 +113,7 @@ export const SignUp = () => {
                             </div>
                             <button
                                 type="submit"
-                                className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                className="flex flex-row items-center justify-between w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                             >
                                 <div className={`${!isLoading ? 'invisible' : 'visible'}`}>
                                     <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-blue-300 fill-blue-100" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
