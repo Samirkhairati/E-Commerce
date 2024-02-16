3f6e5731433a96113c3a32b32588ace7ca657725
Samirkhairati <samirk10101@gmail.com>
2024-02-16 15:19:41 +0530

    test: jwt security compromise

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,9 +7,9 @@ const generateToken = (res, userId) => {
     const token = jwt.sign(payload, secretKey, options);
 
     res.cookie('jwt', token, {
-        httpOnly: true,
+        //httpOnly: true,
         // secure: process.env.NODE_ENV === 'production',
-        sameSite: "strict",
+        //sameSite: "strict",
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     });
 
