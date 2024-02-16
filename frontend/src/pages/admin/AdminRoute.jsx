901728c808e79bfb082ecebe450381ca0c93ecc3
Samirkhairati <samirk10101@gmail.com>
2024-02-17 02:19:48 +0530

    test: disabling admin requirements

--- frontend/src/pages/admin/AdminRoute.jsx
+++ frontend/src/pages/admin/AdminRoute.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 
 const AdminRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  return userInfo && userInfo.isAdmin ?
+  return userInfo && /*userInfo.isAdmin*/1 ?
     (
       <>
         <Outlet />
