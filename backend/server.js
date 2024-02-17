88c3b762007a793095e02f076a8648720952e656
Samirkhairati <samirk10101@gmail.com>
2024-02-18 04:35:46 +0530

    test: allow headers

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token, X-Key, x-access-token, x-key, x-auth-token, x-auth-key, x-auth');
     res.header('Access-Control-Allow-Credentials', true);
     next();
   })
@@ -44,7 +44,6 @@ app.use(cors(
         credentials: true,
      }
 ));
-app.options('*', cors());
 
 // routes
 app.use("/api/users", userRoutes);
