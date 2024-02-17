e12546c049172cc34fe7a4eb8a9d13dee5fb3662
Samirkhairati <samirk10101@gmail.com>
2024-02-18 03:52:11 +0530

    test: cors credentials true

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(cors(
         // origin: 'http://localhost:3000', Build URL
         origin: true,
        // origin:  process.env.FRONTEND_URL || 'http://localhost:5173',
-        credentials: include,
+        credentials: true,
      }
 ));
 app.options('*', cors());
