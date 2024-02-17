9165d5db6db0ff2bbc8993484ef24332890e307e
Samirkhairati <samirk10101@gmail.com>
2024-02-18 02:53:00 +0530

    test: cross domain cookies removed

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(crossDomainCookies)
+//app.use(crossDomainCookies)
 app.use(cors(
      {
         // origin: 'https://e-commerce-production-ecfb.up.railway.app', Railway URL
