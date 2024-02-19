346731b9c1dc7b0c880d970122030f21490ae759
Samirkhairati <samirk10101@gmail.com>
2024-02-19 19:03:06 +0530

    build: rendering built frontend within backend server

--- backend/server.js
+++ backend/server.js
@@ -16,43 +16,41 @@ import orderRoutes from './routes/orderRoutes.js';
 // setup
 dotenv.config();
 connectDB();
+const app = express();
+const __dirname = path.resolve();
 
 // middleware
-const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-	cors({
-		origin: function (origin, callback) {
-			// allow requests with no origin
-			// (like mobile apps or curl requests)
-			if (!origin) return callback(null, true);
-			if (['http:localhost:5173','http:localhost:3000','https://e-commerce-production-ecfb.up.railway.app'].indexOf(origin) === -1) {
-				var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(cors(
+     {
+        // origin: 'https://e-commerce-production-ecfb.up.railway.app', Railway URL
+        // origin: 'http://localhost:5173', //Development URL
+        // origin: 'http://localhost:3000', Build URL
+        origin: true,
+       // origin:  process.env.FRONTEND_URL || 'http://localhost:5173',
+        credentials: true,
+     }
+));
 
 // routes
-
-
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
+
+// TEST
 app.get('/', (req, res) => {
     res.send(`API is running on port ${process.env.PORT}...`);
 })
-
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(process.env.PORT || 6969, () => {
     console.log(`Server running on port ${process.env.PORT || 6969}`);
