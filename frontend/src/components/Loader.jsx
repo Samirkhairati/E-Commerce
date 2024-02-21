683954f77e7dc0c7292209b0df14bf2a2fd11734
Samirkhairati <samirk10101@gmail.com>
2024-02-21 21:07:49 +0530

    test: origin test

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,17 +1,7 @@
 const Loader = () => {
   return (
-<<<<<<< HEAD
     <span className="loader"></span>
-=======
-    <>
-      <span className="loader"></span>
-    </>
->>>>>>> 7cac309 (feat: update current user)
   );
 };
 
 export default Loader;
-<<<<<<< HEAD
- 
-=======
->>>>>>> 7cac309 (feat: update current user)
