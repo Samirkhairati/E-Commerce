b792168b3a0a356b6d618b3e56eb4f56bc075324
Samirkhairati <samirk10101@gmail.com>
2024-02-21 22:39:34 +0530

    fix: loader

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,17 +1,9 @@
 const Loader = () => {
   return (
-<<<<<<< HEAD
-    <span className="loader"></span>
-=======
     <>
       <span className="loader"></span>
     </>
->>>>>>> d9dd0928616556dc5f0f060f17c0ef8de6c6bc99
   );
 };
 
 export default Loader;
-<<<<<<< HEAD
-=======
-   
->>>>>>> d9dd0928616556dc5f0f060f17c0ef8de6c6bc99
